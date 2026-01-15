efaacfe73d1abc5e246d97a0047dabed40bf515c
Gerard <gerartph@gmail.com>
2026-01-15 18:35:48 +0100

    Eliminar WorkingHours antiguo y similares

--- src/main/java/org/example/ultimatecalendarmaven/repository/TimeOffRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.ultimatecalendarmaven.repository;
-import org.example.ultimatecalendarmaven.model.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.time.OffsetDateTime;
-import java.util.*;
-import java.util.UUID;
-public interface TimeOffRepository extends JpaRepository<TimeOff, UUID> {
-    List<TimeOff> findByStaffAndStartsAtLessThanEqualAndEndsAtGreaterThanEqual(Staff staff, OffsetDateTime ends, OffsetDateTime starts);
-}
--- src/main/java/org/example/ultimatecalendarmaven/repository/WorkingHoursRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.ultimatecalendarmaven.repository;
-import org.example.ultimatecalendarmaven.model.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.*;
-import java.util.UUID;
-public interface WorkingHoursRepository extends JpaRepository<WorkingHours, UUID> {
-    List<WorkingHours> findByStaffAndWeekdayOrderByStartTimeAsc(Staff staff, int weekday);
-}
