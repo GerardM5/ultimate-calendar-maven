992abc4955e85d2ea53ab3bd55d8d574ab2d0ee2
Gerard <gerartph@gmail.com>
2026-01-23 21:44:45 +0100

    AppointmentSummaryDTO con status y notes

--- src/main/java/org/example/ultimatecalendarmaven/dto/AppointmentSummaryDTO.java
+++ src/main/java/org/example/ultimatecalendarmaven/dto/AppointmentSummaryDTO.java
@@ -1,6 +1,8 @@
 package org.example.ultimatecalendarmaven.dto;
 
 import lombok.*;
+import org.example.ultimatecalendarmaven.model.AppointmentStatus;
+
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -14,6 +16,8 @@ public class AppointmentSummaryDTO {
     private StaffResponseDTO staff;
     private CustomerResponseDTO customer;
     private ServiceResponseDTO service;
+    private AppointmentStatus status;
+    private String notes;
     private OffsetDateTime startsAt;
     private OffsetDateTime endsAt;
 }
