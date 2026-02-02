a3ec41998d76cfff8d6fb77d21c278cb5c8e147a
Gerard <gerartph@gmail.com>
2026-02-02 17:33:17 +0100

    schedules ok

--- src/main/java/org/example/ultimatecalendarmaven/controller/StaffScheduleController.java
+++ src/main/java/org/example/ultimatecalendarmaven/controller/StaffScheduleController.java
@@ -17,22 +17,22 @@ import java.util.UUID;
 @Tag(name = "Staff Schedule", description = "APIs for managing staff schedules")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/tenants/{tenantId}/staff/{staffId}/schedule")
+@RequestMapping("/api/v1/schedules")
 public class StaffScheduleController {
 
     @Autowired
     private final StaffScheduleService staffScheduleService;
 
     @PostMapping
-    public ResponseEntity<?> assign(@PathVariable("tenantId") UUID tenantId,
-                                    @PathVariable("staffId") UUID staffId,
+    public ResponseEntity<?> assign(@RequestHeader(name = "X-Tenant-ID") UUID tenantId,
+                                    @RequestParam("staffId") UUID staffId,
                                     @RequestBody List<StaffScheduleRequestDTO> staffScheduleRequestDTOList) {
         List<StaffScheduleResponseDTO> response = staffScheduleService.assignSchedule(tenantId, staffId, staffScheduleRequestDTOList);
         return ResponseEntity.status(201).body(response);
     }
 
     @GetMapping()
-    public ResponseEntity<?> list(@PathVariable UUID tenantId,
+    public ResponseEntity<?> list(@RequestHeader(name = "X-Tenant-ID") UUID tenantId,
                                   @RequestParam(required = false) List<UUID> staffId,
                                   //from and to params
                                   @RequestParam(required = false) String from,
@@ -42,8 +42,8 @@ public class StaffScheduleController {
     }
 
     @PutMapping
-    public ResponseEntity<?> update(@PathVariable UUID tenantId,
-                                    @PathVariable UUID staffId,
+    public ResponseEntity<?> update(@RequestHeader(name = "X-Tenant-ID") UUID tenantId,
+                                    @RequestParam UUID staffId,
                                     @RequestBody List<StaffScheduleRequestUpdateDTO> staffScheduleRequestDTOList) {
 
         return ResponseEntity.ok(
